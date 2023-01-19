e9fca9d5cbbc17a168c1c61fcd3fbc946de17e25
camilagx <camilac.gomez97@gmail.com>
2023-01-19 12:14:40 -0700

    Double char solved

--- 8kyu-challenges/double-char-solution.js
+++ 8kyu-challenges/double-char-solution.js
@@ -8,3 +8,13 @@ Examples:
 *"1234!_" -> "11223344!!__  "
 
 */
+function doubleChar(str) {
+  let newString = "";
+  
+  for(let i =0; i < str.length; i++){
+    newString += str[i];
+    newString += str[i];
+  }
+  
+  return newString;
+}
