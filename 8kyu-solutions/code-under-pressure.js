9506a1260448672b0b389d5aac3a1aa9dbc2c0de
camilagx <camilac.gomez97@gmail.com>
2023-01-19 13:35:44 -0700

    Code under pressure solution added

---
+++ 8kyu-solutions/code-under-pressure.js
@@ -0,0 +1,10 @@
+/* Challenge:
+You need to double the integer and return it.
+*/
+
+//First Solution & best-practice
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
