aca2210450bc2ad5497dc2be5c3aab13e777f813
camilagx <camilac.gomez97@gmail.com>
2023-02-01 14:45:27 -0700

    Starter code added

--- js-practice/balloon-attack.js
+++ js-practice/balloon-attack.js
@@ -16,5 +16,24 @@ Write a game function balloonAttack that takes two Player instances, calculates 
 
 You can test your balloonAttack function by creating two instances of the Player class to use as arguments for your function like below:
 
-
+const p1 = new Player('p1', 5);
+const p2 = new Player('p2', 2);
+ 
+balloonAttack(p1, p2);
+Feel free to use the status() method to output each player’s balloon count at any given time.
 */
+
+//Starter code
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
+  }
+}
+
+// Write function below
