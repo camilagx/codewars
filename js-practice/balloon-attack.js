4c5d6d526e11d7b97594dc4e0205406a46d4af56
camilagx <camilac.gomez97@gmail.com>
2023-02-01 18:02:51 -0700

    added balloon-attack solution from codecademy

--- js-practice/balloon-attack.js
+++ js-practice/balloon-attack.js
@@ -23,7 +23,7 @@ balloonAttack(p1, p2);
 Feel free to use the status() method to output each player’s balloon count at any given time.
 */
 
-//Solution
+//Personal Solution
 class Player {
   constructor(name, hitsPerMinute) {
     this.name = name;
@@ -49,10 +49,37 @@ function balloonAttack(player1, player2){
   }
 }
 
-
+//Test code
 const p1 = new Player('p1', 5);
 const p2 = new Player('p2', 2);
  
 balloonAttack(p1, p2);
 
+// Codecademy solution
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
+  }
+}
+
 // Write function below
+const balloonAttack = (player1, player2) => {
+  for (let i = 1; i <= 10; i++){
+    player2.balloonCount -= player1.hitsPerMinute
+    player1.balloonCount -= player2.hitsPerMinute
+    player1.status();
+    player2.status();
+  }
+  if (player1.balloonCount > player2.balloonCount) {
+    return player1.name;
+  }else if (player2.balloonCount > player1.balloonCount)
+    return player2.name
+
+  return 'Tie'
+}
