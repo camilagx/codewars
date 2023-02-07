f3e22f870496b9582edd45ee0fcb5f046374f191
camilagx <camilac.gomez97@gmail.com>
2023-02-06 22:48:16 -0700

    Starter code added

--- js-practice/practice-json.js
+++ js-practice/practice-json.js
@@ -4,3 +4,6 @@ Create a variable called jsObject that is an object parsed from jsonData.
 Print out the array of all the children property nested in jsObject. Be sure to use either bracket notation or dot notation to access the nested properties.
 
 */
+
+//Starter Code
+const jsonData = '{ "parent": { "name": "Sally", "age": 45, "children" : [ { "name": "Kim", "age": 3 }, { "name": "Lee", "age": 1 } ] } }';
