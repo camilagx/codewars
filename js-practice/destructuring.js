5d0e26c27d95198d32576af3625d56ab2e219cee
camilagx <camilac.gomez97@gmail.com>
2023-02-07 16:26:45 -0700

    Starter code added

--- js-practice/destructuring.js
+++ js-practice/destructuring.js
@@ -2,3 +2,11 @@
 Destructure the items from the colors array into new variables color1, color2, and color3. If done correctly, the colors should be printed in this order: ‘blue’, ‘red’, ‘purple’.
 
 */
+
+//Starter Code
+let colors = ['blue','red','purple'];
+
+// Destructure here
+
+
+console.log(color1, color2, color3);
