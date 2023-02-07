7ac1adfdccf8d35c103d3765498efa1f7c18d4b0
camilagx <camilac.gomez97@gmail.com>
2023-02-07 16:30:32 -0700

    Challenge #2 added

--- js-practice/destructuring.js
+++ js-practice/destructuring.js
@@ -1,4 +1,4 @@
-/* Challenge:
+/* Challenge #1:
 Destructure the items from the colors array into new variables color1, color2, and color3. If done correctly, the colors should be printed in this order: ‘blue’, ‘red’, ‘purple’.
 
 */
@@ -10,3 +10,8 @@ let colors = ['blue','red','purple'];
 //Solution
 let [color1, color2, color3] = colors;
 console.log(color1, color2, color3);
+
+/* Challenge #2:
+Destructure the items from the planets object so that they are named x, y, and z. If done correctly, the planets should be printed in this order: ‘Saturn’, ‘Mars’, ‘Neptune’.
+
+*/
