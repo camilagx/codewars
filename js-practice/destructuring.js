38f397ac3654ae5c1f6626f175ede7300c75f3b7
camilagx <camilac.gomez97@gmail.com>
2023-02-07 16:26:27 -0700

    Challenge added

---
+++ js-practice/destructuring.js
@@ -0,0 +1,4 @@
+/* Challenge:
+Destructure the items from the colors array into new variables color1, color2, and color3. If done correctly, the colors should be printed in this order: ‘blue’, ‘red’, ‘purple’.
+
+*/
